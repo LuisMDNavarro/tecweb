430b1ec5e5ae35781cf9800b0f9142eaa0f7155f
lucky <luis.diaz.navarro.210@gmail.com>
2023-09-16 17:23:46 -0600

    Correccion, separado de funcion del ejercicio 1

--- practicas/p04/index.php
+++ practicas/p04/index.php
@@ -5,21 +5,18 @@
     <title>Práctica 4</title>
 </head>
 <body>
+    <?php
+    require_once __DIR__.'/p04_funciones.php';
+    ?>
     <h2>Ejercicio 1</h2>
     <p>Escribir programa para comprobar si un número es un múltiplo de 5 y 7</p>
     <?php
         if(isset($_GET['numero']))
         {
             $num = $_GET['numero'];
-            if ($num%5==0 && $num%7==0)
-            {
-                echo '<h3>R= El número '.$num.' SÍ es múltiplo de 5 y 7.</h3>';
-            }
-            else
-            {
-                echo '<h3>R= El número '.$num.' NO es múltiplo de 5 y 7.</h3>';
-            }
+            echo multipo($num);
         }
     ?>
+    <h2>Ejercicio 2</h2>
 </body>
 </html>
