6f9f7754393e95a56e55e798ade628be2af94a68
lucky <luis.diaz.navarro.210@gmail.com>
2023-09-26 15:37:09 -0600

    Ejercicio 5 terminado y carga de imagenes

---
+++ practicas/p06/get_productos_vigentes.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
+	<?php
+            $eliminado = 0;
+            /** SE CREA EL OBJETO DE CONEXION */
+            @$link = new mysqli('localhost', 'root', 'Luis1', 'marketzone');
+            /** NOTA: con @ se suprime el Warning para gestionar el error por medio de código */
+    
+            /** comprobar la conexión */
+            if ($link->connect_errno) 
+            {
+                die('Falló la conexión: '.$link->connect_error.'<br/>');
+                //exit();
+            }
+    
+            /** Crear una tabla que no devuelve un conjunto de resultados */
+            if ( $result = $link->query("SELECT * FROM productos WHERE eliminado = 0") ) 
+            {
+                /** Se extraen las tuplas obtenidas de la consulta */
+                $row = $result->fetch_all(MYSQLI_ASSOC);
+    
+                /** útil para liberar memoria asociada a un resultado con demasiada información */
+                $result->free();
+            }
+    
+            $link->close();
+        ?>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<title>Producto</title>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	</head>
+	<body>
+		<h3>PRODUCTO</h3>
+
+		<br/>
+		
+		<?php if( isset($row) ) : ?>
+
+			<table class="table">
+				<thead class="thead-dark">
+					<tr>
+					<th scope="col">#</th>
+					<th scope="col">Nombre</th>
+					<th scope="col">Marca</th>
+					<th scope="col">Modelo</th>
+					<th scope="col">Precio</th>
+					<th scope="col">Unidades</th>
+					<th scope="col">Detalles</th>
+					<th scope="col">Imagen</th>
+					</tr>
+				</thead>
+				<tbody>
+                    <?php
+                    foreach ($row as $fila) {
+					echo "<tr>";
+						echo "<th scope='row'>" . $fila['id'] . " </th>";
+						echo "<td>" .$fila['nombre']. "</td>";
+						echo "<td>".$fila['marca']."</td>";
+						echo "<td>" .$fila['modelo'] ."</td>";
+						echo "<td>".$fila['precio']."</td>";
+						echo "<td>". $fila['unidades']."</td>";
+						echo "<td>" .utf8_encode($fila['detalles'])."</td>";
+						echo "<td><img src=".$fila['imagen']." style = 'max-width: 300px;'  ></td>";
+					echo "</tr>";
+                    }
+                    ?>
+				</tbody>
+			</table>
+		<?php endif; ?>
+	</body>
+</html>
