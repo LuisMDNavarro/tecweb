402bae9f0024c0dece00f3751d47fabbb0b3b7b2
lucky <luis.diaz.navarro.210@gmail.com>
2023-10-03 20:37:15 -0600

    Ejercicio 4 completado

---
+++ practicas/p07/actualizar_producto.php
@@ -0,0 +1,28 @@
+<?php
+$id = $_POST['id'];
+$nombre = $_POST['nombre'];
+$marca  = $_POST['marca'];
+$modelo = $_POST['modelo'];
+$precio = $_POST['precio'];
+$detalles = $_POST['detalles'];
+$unidades = $_POST['unidades'];
+$imagen   = $_POST['img'];
+
+@$link = new mysqli('localhost', 'root', 'Luis1', 'marketzone');	
+
+
+
+if ($link->connect_errno) 
+{
+    die('Falló la conexión: '.$link->connect_error.'<br/>');
+}
+
+$sql = "UPDATE productos SET  nombre = '$nombre', marca = '$marca', modelo = '$modelo',  precio = '$precio', detalles = '$detalles', unidades = '$unidades', imagen = '$imagen'  WHERE id= $id ";
+if(mysqli_query($link, $sql)){
+    echo "Registro actualizado.";
+} else {
+    echo "ERROR: No se ejecuto $sql. " . mysqli_error($link);
+}
+                
+$link->close();
+?>
