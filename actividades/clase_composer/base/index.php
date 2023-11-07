c977792ca95aceabacebf1e87a65c74729969e27
lucky <luis.diaz.navarro.210@gmail.com>
2023-11-07 14:09:36 -0600

    Base proporcionada por el profesor

---
+++ actividades/clase_composer/base/index.php
@@ -0,0 +1,7 @@
+<?php
+    require_once __DIR__ . '/app/models/User.php';
+    require_once __DIR__ . '/app/views/UserTemplate.php';
+    require_once __DIR__ . '/app/controllers/UserController.php';
+
+    $user = new UserTemplate();
+?>
